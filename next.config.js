32c7fb7b29fa917ab8a28d32f0406bdba630d35c
mendsalbert <mendsalbert@gmail.com>
2024-05-25 09:14:22 +0000

    delete 2 files and update 2 files

--- next.config.js
+++ next.config.js
@@ -33,7 +33,6 @@ const nextConfig = {
     ],
     disableStaticImages: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -43,30 +42,6 @@ const nextConfig = {
   },
 
   ignoreBuildErrors: true,
-  webpackConfig: {
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
-        {
-          test: /\.css$/,
-          loader: "style-loader!css-loader?modules",
-        },
-        {
-          test: /\.(jpg|png|svg)$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 25000,
-            },
-          },
-        },
-      ],
-    },
-  },
 };
 
 module.exports = nextConfig;
