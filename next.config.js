41ab64532a2e5434c046c2929daf0f6077d71caa
mendsalbert <mendsalbert@gmail.com>
2024-05-28 09:18:07 +0000

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -36,6 +36,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  ignoreBuildErrors: true,
+  reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 module.exports = nextConfig;
